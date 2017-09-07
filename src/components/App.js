c8ba0c309e0f0f067da1d58a5c0963a7344b7326
Никита Яблоков <danger@torman.ru>
2017-09-07 23:29:28 +0300

    fix footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {getCurrentUser} from '../common/ajaxRequests';
-import {userData} from '../actionCreators';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getCurrentUser } from '../common/ajaxRequests';
+import { userData } from '../actionCreators';
 
 import '../common/validationRules';
 import Header from './Header';
@@ -9,45 +9,44 @@ import Footer from './Footer';
 import Preloader from './Preloader';
 
 class App extends Component {
-    componentWillMount() {
-        const token = localStorage.getItem("token");
-        if (token != null) {
-            console.log('АХТУНГ!!! АХТУНГ!! ОБНАРУЖЕН ТОКЕН!!');
-            getCurrentUser();
-        } else {
-            console.log('ТОКЕН НЕ ОБНАРУЖЕН!!!');
-            this.props.resetUserData(); // Need for dell the temporal property userData.loaded
-        }
+  componentWillMount() {
+    const token = localStorage.getItem('token');
+    if (token != null) {
+      console.log('АХТУНГ!!! АХТУНГ!! ОБНАРУЖЕН ТОКЕН!!');
+      getCurrentUser();
+    } else {
+      console.log('ТОКЕН НЕ ОБНАРУЖЕН!!!');
+      this.props.resetUserData(); // Need for dell the temporal property userData.loaded
     }
+  }
 
-    render() {
-        return (
-            <div>
-                <Header/>
-                {
-                    this.props.state.userData.loaded !== false ?
-                        <span>
-                            {this.props.children}
-                            <Footer/>
-                        </span>
-                        :
-                        <span>
-                            <Preloader/>
-                            <Footer/>
-                        </span>
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        {this.props.state.userData.loaded !== false ? (
+          <div>{this.props.children}</div>
+        ) : (
+          <div
+            style={{
+              minHeight: '100vh'
+            }}>
+            <Preloader />
+          </div>
+        )}
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default connect(
-    state => ({
-        state: state
-    }),
-    dispatch => ({
-        resetUserData(){
-            dispatch(userData.updateUser({}))
-        }
-    })
+  state => ({
+    state: state
+  }),
+  dispatch => ({
+    resetUserData() {
+      dispatch(userData.updateUser({}));
+    }
+  })
 )(App);
--- src/reducers/userData.js
+++ src/reducers/userData.js
@@ -1,31 +1,35 @@
-import { userData } from "../actionCreators";
-import { createReducer } from "redux-act";
+import { userData } from '../actionCreators';
+import { createReducer } from 'redux-act';
 import { correctImg } from '../common/helpers';
+import { imgUrl } from '../common/ajaxRequests';
 
-export default createReducer({
-  [userData.updateUser]: (state, payload) => {
-    let newUserData = Object.assign({}, payload);
-    if (newUserData.avatar === null || !newUserData.avatar) {
-      newUserData.avatar = '/img/avatar.png';
-    } else if (newUserData.avatar !== null || newUserData.avatar) {
-      newUserData.avatar = '//emploicore.lod-misis.ru/images/' + newUserData.avatar;
+export default createReducer(
+  {
+    [userData.updateUser]: (state, payload) => {
+      let newUserData = Object.assign({}, payload);
+      if (newUserData.avatar === null || !newUserData.avatar) {
+        newUserData.avatar = '/img/avatar.png';
+      } else if (newUserData.avatar !== null || newUserData.avatar) {
+        newUserData.avatar = imgUrl + newUserData.avatar;
+      }
+      if (newUserData.portfolio) {
+        for (var i = 0; i < newUserData.portfolio.length; i++) {
+          newUserData.portfolio[i].avatar = correctImg(newUserData.portfolio[i].avatar);
+        }
+      }
+      //DELL
+      if (newUserData.contacts === null) {
+        newUserData.contacts = [];
+      }
+      if (newUserData.organizations === null) {
+        newUserData.organizations = [];
+      }
+      if (newUserData.portfolio === null) {
+        newUserData.portfolio = [];
+      }
+      //DELL
+      return newUserData;
     }
-    if (newUserData.portfolio) {
-      for (var i = 0; i < newUserData.portfolio.length; i++) {
-      newUserData.portfolio[i].avatar = correctImg(newUserData.portfolio[i].avatar);
-    }
-    }
-    //DELL
-    if (newUserData.contacts === null) {
-      newUserData.contacts = [];
-    }
-    if (newUserData.organizations === null) {
-      newUserData.organizations = [];
-    }
-    if (newUserData.portfolio === null) {
-      newUserData.portfolio = [];
-    }
-    //DELL
-    return newUserData;
-  }
-}, { loaded: false });
+  },
+  { loaded: false }
+);
